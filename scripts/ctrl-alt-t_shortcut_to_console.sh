d4ddfbb398a3b861450c95cdb73bdebc7c512bd4
h0mikus <fatrus.s0dtl@gmail.com>
2017-06-18 14:39:40 +0600

    Create ctrl-alt-t_shortcut_to_console.sh

---
+++ scripts/ctrl-alt-t_shortcut_to_console.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Make schortcut to gnome-terminal for Kali Linux"
+echo
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'gnome-terminal'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'gnome-terminal'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Primary><Alt>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+echo
+echo "Now you can press 'CTRL+ALT+t(T)' to open new terminal window"
