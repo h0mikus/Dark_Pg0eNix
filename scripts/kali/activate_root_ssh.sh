357259bc76bf42ec77a17e29252fbec040ecc801
h0mikus <fatrus.s0dtl@gmail.com>
2017-06-18 15:02:35 +0600

    Update activate_root_ssh.sh
    add descriptions

--- scripts/kali/activate_root_ssh.sh
+++ scripts/kali/activate_root_ssh.sh
@@ -4,12 +4,21 @@ echo
 echo "Stopping SSH"
 echo
 service ssh stop
+# Script stop ssh daemon
 
 grep -r "#PermitRootLogin prohibit-password" /etc/ssh/sshd_config &> /dev/null
+# Check if there is a record in config file
 
 if [ $? == 0 ]; then
+# Compare resul. If result = 0 then config file have record "#PermitRootLogin prohibit-password" and script change it. If resul = 1,
+# script skip change
+
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+# Make config backup!!! Every time make backup config, it easy way to live withou problems
+
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# Script replace record to activate ssh session with "root" login!!! Do it if use great password at root account or you use linux at VM
+
 echo "Permition changed. Now try connect to 22 port"
 echo
 else
@@ -22,6 +31,7 @@ fi
 echo "Activating SSH Service"
 echo
 systemctl is-enable ssh &> /dev/null
+# Script checking start ssh with system. If no then change value to start
 if [ $? == 0 ]; then
 echo "Service is already activated"
 else
@@ -30,3 +40,4 @@ echo "Service activated"
 fi
 
 service ssh start
+# Script start ssh daemon
